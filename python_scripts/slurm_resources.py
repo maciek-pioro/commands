cce01c7c700b69d6efa4542015369e0032a326cc
Maciej Pióro <70006947+maciek-pioro@users.noreply.github.com>
2023-11-15 14:01:47 +0100

    Create slurm_resources.py

---
+++ python_scripts/slurm_resources.py
@@ -0,0 +1,76 @@
+def parse_resource_allocations(resource_entries):
+    """
+    Parse resource allocation strings and convert them into dictionaries with keys being resource types and values
+    being the allocated amounts. This function also handles conversion of different units (e.g., from GB to MB).
+
+    :param resource_entries: A list of strings where each string contains comma-separated 'key=value' pairs.
+    :return: A list of dictionaries with resource types and available amounts.
+    """
+    resource_dicts = []
+
+    # Define a helper function to parse resource values and convert G (gigabyte) to M (megabyte) if necessary.
+    def parse_resource_value(value):
+        if value.endswith('G'):
+            return int(value.rstrip('G')) * 1024  # Convert from GB to MB
+        elif value.endswith('M'):
+            return int(value.rstrip('M'))
+        else:
+            return int(value)
+
+    # Process each entry
+    for entry in resource_entries:
+        resource_dict = {}
+        resources = entry.split(',')  # Split the entry into 'key=value' components
+        for r in resources:
+            key, value = r.split('=')
+            resource_dict[key] = parse_resource_value(value)
+        resource_dicts.append(resource_dict)
+
+    return resource_dicts
+
+def calculate_available_resources(cfg_tres_list, alloc_tres_list):
+    """
+    Calculate available resources by subtracting allocated resources from config (total) resources.
+
+    :param cfg_tres_list: A list of dictionaries containing total resources from CfgTRES field.
+    :param alloc_tres_list: A list of dictionaries containing allocated resources from AllocTRES field.
+    :return: A list of dictionaries with available resources for each node.
+    """
+    available_resources_list = []
+
+    # Iterate through the resources and calculate differences.
+    for cfg_tres, alloc_tres in zip(cfg_tres_list, alloc_tres_list):
+        # Create a dictionary to hold available resources for the current node.
+        available_resources = {}
+        for resource, total in cfg_tres.items():
+            allocated = alloc_tres.get(resource, 0)  # Default to 0 if not allocated
+            available_resources[resource] = total - allocated
+
+        available_resources_list.append(available_resources)
+
+    return available_resources_list
+
+# Extract and parse the CfgTRES and AllocTRES fields from the nodes output
+cfg_tres = [
+    "cpu=256,mem=1960860M,billing=1247,gres/gpu=14",
+    "cpu=256,mem=1960860M,billing=863,gres/gpu=8"
+]
+alloc_tres = [
+    "cpu=160,mem=1288G,gres/gpu=7",
+    "cpu=76,mem=578304M,gres/gpu=6"
+]
+
+# Parse the resource allocations
+cfg_tres_list = parse_resource_allocations(cfg_tres)
+alloc_tres_list = parse_resource_allocations(alloc_tres)
+
+# Calculate available resources for each node
+available_resources = calculate_available_resources(cfg_tres_list, alloc_tres_list)
+
+# Print the results
+node_names = ["gpu01", "login01"]
+for node, resources in zip(node_names, available_resources):
+    print(f"Node: {node}")
+    for res, avail in resources.items():
+        print(f"{res.upper()}: {avail}")
+    print()
